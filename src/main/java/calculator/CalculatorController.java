76da555cfac363925cdb5ca38ff6ce033ca1933f
katya <kotenokches@rambler.ru>
2022-03-23 15:50:36 +0500

    Первый коммит

---
+++ src/main/java/calculator/CalculatorController.java
@@ -0,0 +1,39 @@
+package calculator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+    private CalculatorServiсe calculatorServiсe = new CalculatorServiсe();
+
+    public CalculatorController(CalculatorServiсe calculatorServiсe) {
+        this.calculatorServiсe = calculatorServiсe;
+    }
+
+    @GetMapping(path = "/calculator")
+    public String Calculator() {
+        return calculatorServiсe.Calculator();
+    }
+
+    @GetMapping(path = "/calculator/plus")
+    public String CalculatorPlus(@RequestParam("num1") Integer num1, @RequestParam("num2") Integer num2) {
+        return calculatorServiсe.CalculatorPlus(num1, num2);
+    }
+
+    @GetMapping(path = "/calculator/minus")
+    public String CalculatorMinus(@RequestParam("num1") Integer num1, @RequestParam("num2") Integer num2) {
+        return calculatorServiсe.CalculatorMinus(num1, num2);
+    }
+
+    @GetMapping(path = "/calculator/multiply")
+    public String CalculatorMultiply(@RequestParam("num1") Integer num1, @RequestParam("num2") Integer num2) {
+        return calculatorServiсe.CalculatorMultiply(num1, num2);
+    }
+
+    @GetMapping(path = "/calculator/divide")
+    public String CalculatorDivide(@RequestParam("num1") Integer num1, @RequestParam("num2") Integer num2) {
+        return calculatorServiсe.CalculatorDivide(num1, num2);
+    }
+}
---
+++ src/main/java/calculator/CalculatorServiсe.java
@@ -0,0 +1,34 @@
+package calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiсe {
+    public String Calculator() {
+        return "<b><i>Добро пожаловать в калькулятор</i></b>";
+    }
+
+    public String CalculatorPlus(Integer num1, Integer num2) {
+        int sum = num1 + num2;
+        return num1 + "+" + num2 + "=" + sum;
+    }
+
+    public String CalculatorMinus(Integer num1, Integer num2) {
+        int minus = num1 - num2;
+        return num1 + "-" + num2 + "=" + minus;
+    }
+
+    public String CalculatorMultiply(Integer num1, Integer num2) {
+        int multiply = num1 * num2;
+        return num1 + "*" + num2 + "=" + multiply;
+    }
+
+    public String CalculatorDivide(Integer num1, Integer num2) {
+        if (num2 == 0) {
+            return "Деление на 0 запрещено!";
+        } else {
+            int divide = num1 / num2;
+            return num1 + "/" + num2 + "=" + divide;
+        }
+    }
+}
