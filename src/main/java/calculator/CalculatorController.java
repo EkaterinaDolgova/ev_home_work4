2daae56a11b2785a2c052c3f7af4ab5c74f149de
katya <kotenokches@rambler.ru>
2022-03-25 08:32:30 +0500

    Выполнена

--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -25,7 +25,7 @@ public class CalculatorController {
     @GetMapping(path = "/calculator/minus")
     public String CalculatorMinus(@RequestParam("num1") Integer num1, @RequestParam("num2") Integer num2) {
         return calculatorServiсe.CalculatorMinus(num1, num2);
-    }
+    Ъ
 
     @GetMapping(path = "/calculator/multiply")
     public String CalculatorMultiply(@RequestParam("num1") Integer num1, @RequestParam("num2") Integer num2) {
